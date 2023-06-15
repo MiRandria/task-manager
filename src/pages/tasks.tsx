99d5c9d89b53ab1bba24743229dbfabf72caa54b
MiRandria <hei.minosoa@gmail.com>
2023-06-15 11:33:15 +0300

    feat: add crud in task

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,61 +1,67 @@
+import useTaskManager from '@/store/useTaskManager';
 import React, { ChangeEvent, useRef } from 'react';
 
 interface Task {
-  id: number,
-  title: string,
-  completed: boolean,
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
 const TaskManager = () => {
-  // const createTaskRef = ...:
-  // const {
-  //   tasks,
-  //   searchTask,
-  //   addTask,
-  //   updateTask,
-  //   deleteTask,
-  //   setSearchTask,
-  // } = useTaskManager();
+  const createTaskRef = useRef<HTMLInputElement>(null);
+  const {
+    tasks,
+    searchTasks,
+    addTask,
+    updateTask,
+    deleteTask,
+    setSearchTask,
+  } = useTaskManager();
+  
+  const searchValue = useRef<string>(''); // Variable pour stocker la valeur de recherche
 
   const handleAddTask = () => {
-    const title = ""; // Replace with the value in the createTaskRef 
-    const newTask = {
-      id: Date.now(),
-      title,
-      completed: false,
-    };
-    // addTask(newTask);
+    if (createTaskRef.current) {
+      const title = createTaskRef.current.value;
+      const newTask: Task = {
+        id: Date.now(),
+        title,
+        completed: false,
+      };
+      addTask(newTask);
+      createTaskRef.current.value = '';
+    }
   };
 
-  const handleUpdateTask = (taskId: number, updatedTask: Task) => {
-    // updateTask(taskId, updatedTask);
+  const handleUpdateTask = (taskId: number, updatedTask: Partial<Task>) => {
+    updateTask(taskId, updatedTask);
   };
 
   const handleDeleteTask = (taskId: number) => {
-    // deleteTask(taskId);
+    deleteTask(taskId);
   };
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    // setSearchTask(e.target.value);
+    const searchTerm = e.target.value.toLowerCase();
+    searchValue.current = searchTerm; // Mettre à jour la valeur de recherche
+    setSearchTask(tasks);
   };
 
-  // See! I already give you everything!
-  // const filteredTasks = tasks.filter((task) =>
-  //   task.title.toLowerCase().includes(searchTask.toLowerCase())
-  // );
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(searchValue.current) // Utiliser la valeur de recherche stockée
+  );
 
   return (
     <div>
       <h1>Task Manager</h1>
 
-      <input type="text" /*ref={}*//>
+      <input type="text" ref={createTaskRef} />
 
       <button onClick={handleAddTask}>Add Task</button>
 
       <input type="text" onChange={handleSearch} placeholder="Search Task" />
 
       <ul>
-        {/* 
         {filteredTasks.map((task) => (
           <li key={task.id}>
             <input
@@ -68,7 +74,6 @@ const TaskManager = () => {
             <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
           </li>
         ))}
-        */}
       </ul>
     </div>
   );
--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,54 @@
-const useTaskManager = () => {
+import { SetState, create } from 'zustand';
 
+interface Task {
+  id: number;
+  title: string;
 }
 
-export {
-  useTaskManager
+interface TaskManagerState {
+  tasks: Task[];
+  searchTasks: (searchTerm: string) => void;
+  addTask: (newTask: Task) => void;
+  updateTask: (taskId: number, updatedTask: Partial<Task>) => void;
+  deleteTask: (taskId: number) => void;
+  setSearchTask: (tasks: Task[]) => void;
 }
+
+const useTaskManager = create<TaskManagerState>((set: SetState<TaskManagerState>) => ({
+  tasks: [],
+
+  searchTasks: (searchTerm: string) => {
+    set((state: TaskManagerState) => ({
+      tasks: state.tasks.filter((task: Task) =>
+        task.title.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    }));
+  },
+
+  addTask: (newTask: Task) => {
+    set((state: TaskManagerState) => ({
+      tasks: [...state.tasks, newTask],
+    }));
+  },
+
+  updateTask: (taskId: number, updatedTask: Partial<Task>) => {
+    set((state: TaskManagerState) => ({
+      tasks: state.tasks.map((task: Task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    }));
+  },
+
+  deleteTask: (taskId: number) => {
+    set((state: TaskManagerState) => ({
+      tasks: state.tasks.filter((task: Task) => task.id !== taskId),
+    }));
+  },
+  setSearchTask: (tasks: Task[]) => {
+    set((state: TaskManagerState) => ({
+      tasks: tasks,
+    }));
+  },
+}));
+
+export default useTaskManager;
